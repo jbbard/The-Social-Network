9a53a208d1832e9bfee0e25867c5fabdadfbeb64
Jean-Baptiste BARD <bard@5pm-studio.fr>
2022-06-22 23:08:04 +0200

    add mod a

---
+++ Assets/ModA/ModASystem.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace ModA
+{
+    public partial class ModASystem : SystemBase
+    {
+        protected override void OnCreate()
+        {
+            Debug.LogError("ModA");
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+    }
+}
