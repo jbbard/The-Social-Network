9dd5571cec3ab50800d914c727cdbf4f51ac0e3a
Jean-Baptiste BARD <bard@5pm-studio.fr>
2022-07-02 18:43:10 +0200

    add a function to load mod dlls

--- Assets/Proto/Bootstrap.cs
+++ Assets/Proto/Bootstrap.cs
@@ -1,28 +1,45 @@
-﻿using Unity.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Unity.Entities;
 using UnityEngine;
-
-using Dummy.Archetypes;
-using U_Components = Dummy.Components;
+using Sirenix.OdinInspector;
 
 public class Bootstrap : MonoBehaviour
 {
+    [Button]
+    public static void LoadModDLLs() {
+        Debug.Log("Load mod DLLs");
+
+        var systems = new List<Type>();
+        var assemblies = Directory.GetFiles(Application.streamingAssetsPath + "/Mods", "*.dll", SearchOption.AllDirectories);
+
+        try {
+            foreach (var dll in assemblies) {
+                var assembly = Assembly.LoadFile(dll);
+                Debug.Log($"Load {dll}");
+
+                var types = assembly.GetTypes();
+                foreach (var type in types) {
+                    if (typeof(ComponentSystemBase).IsAssignableFrom(type))
+                        systems.Add(type);
+                }
+            }
+        } catch (Exception e) {
+            Debug.LogException(e);
+        }
+
+        Debug.Log("Add mod systems to world");
+        for (int i = 0; i < systems.Count; i++)
+        {
+            Debug.Log($"{systems[i].Name} added");
+            World.DefaultGameObjectInjectionWorld.CreateSystem(systems[i]);
+        }
+    }
+
     private void Start()
     {
-        var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-        Archetypes.Initialize(entityManager);
-
-        var player = entityManager.CreateEntity(Archetypes.s_player);
-        entityManager.SetComponentData(player, new U_Components.Skills.EnergyMax { value = 50 });
-        entityManager.SetComponentData(player, new U_Components.Skills.LibidoMax { value = 10 });
-        entityManager.SetComponentData(player, new U_Components.Skills.Strength { value = 0 });
-
-        var blueHouse = entityManager.CreateEntity(Archetypes.s_location);
-        entityManager.SetComponentData(blueHouse, new U_Components.Base.Name { value = "Blue House" });
-
-        var whiteHouse = entityManager.CreateEntity(Archetypes.s_location);
-        entityManager.SetComponentData(whiteHouse, new U_Components.Base.Name { value = "White House" });
-
-        var street = entityManager.CreateEntity(Archetypes.s_location);
-        entityManager.SetComponentData(street, new U_Components.Base.Name { value = "Street" });
+        LoadModDLLs();
     }
 }
